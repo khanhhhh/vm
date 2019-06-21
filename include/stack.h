a894539e8854574c6d69519a77d392a9ea4df13f
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2019-06-21 23:21:36 +0800

    fibonacci

--- include/stack.h
+++ include/stack.h
@@ -16,12 +16,11 @@ object	stack_peek(stack *s);
 object	stack_pop(stack *s);
 void	stack_push(stack *s, object item);
 //function call procedure:
-//-1. push return value
-//0. stack_push_fp ()
+//0. stack_push_fp : push return address and frame pointer
 //2. push parameters
-//1. stack_frame_call
+//1. stack_frame_call : set fp sp to new value
 //3. jump / execute code
-//4. stack_frame_return
+//4. stack_frame_return : get return value and reset
 // fp = stack_pop() + stack_pop()
 void	stack_push_fp(stack *s);
 void	stack_call(stack *s, uint32_t ret_addr);
