d49c9ea1f086832b1993d57392f6f741439627e4
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2019-06-22 18:56:23 +0800

    modified:   include/thread.hh

--- cpp/include/thread.hh
+++ cpp/include/thread.hh
@@ -88,262 +88,66 @@ public:
        	{
 		stack_store(0, 0);
 		stack_store(1, 0);
+		// SET HALT
+		for (opcode i=0; i<256; i++) {
+			ops[i] = nullptr;
+		}
+		// 0x0.. : SPECIAL
 		ops[0x00] = &thread::halt;
 		ops[0x01] = &thread::nop;
-		ops[0x02] = &thread::load_code;
-		ops[0x03] = &thread::load_code_array;
-		ops[0x04] = &thread::load_data;
-		ops[0x05] = &thread::load_data_array;
-		ops[0x06] = &thread::load_stack;
-		ops[0x07] = &thread::load_stack_array;
-		ops[0x08] = &thread::store_data;
-		ops[0x09] = &thread::store_data_array;
-		ops[0x0a] = &thread::store_stack;
-		ops[0x0b] = &thread::store_stack_array;
-		ops[0x0c] = &thread::pop;
-		ops[0x0d] = &thread::dup;
-		ops[0x0e] = &thread::swap;
-		ops[0x0f] = &thread::iadd;
-		ops[0x10] = &thread::fadd;
-		ops[0x11] = &thread::isub;
-		ops[0x12] = &thread::fsub;
-		ops[0x13] = &thread::imul;
-		ops[0x14] = &thread::fmul;
-		ops[0x15] = &thread::idiv;
-		ops[0x16] = &thread::fdiv;
-		ops[0x17] = &thread::irem;
-		ops[0x18] = &thread::ineg;
-		ops[0x19] = &thread::fneg;
-		ops[0x1a] = &thread::i2f;
-		ops[0x1b] = &thread::f2i;
-		ops[0x1c] = &thread::jump;
-		ops[0x1d] = &thread::ifeq_jump;
-		ops[0x1e] = &thread::iflt_jump;
-		ops[0x1f] = &thread::ifgt_jump;
-		ops[0x20] = &thread::ifle_jump;
-		ops[0x21] = &thread::ifge_jump;
-		ops[0x22] = &thread::ifne_jump;
-		ops[0x23] = &thread::push_fp;
-		ops[0x24] = &thread::call_fp;
-		ops[0x25] = &thread::return0;
-		ops[0x26] = &thread::return1;
-		ops[0x27] = &thread::return1_array;
-		ops[0x28] = &thread::halt;
-		ops[0x29] = &thread::halt;
-		ops[0x2a] = &thread::halt;
-		ops[0x2b] = &thread::halt;
-		ops[0x2c] = &thread::halt;
-		ops[0x2d] = &thread::halt;
-		ops[0x2e] = &thread::halt;
-		ops[0x2f] = &thread::halt;
-		ops[0x30] = &thread::halt;
-		ops[0x31] = &thread::halt;
-		ops[0x32] = &thread::halt;
-		ops[0x33] = &thread::halt;
-		ops[0x34] = &thread::halt;
-		ops[0x35] = &thread::halt;
-		ops[0x36] = &thread::halt;
-		ops[0x37] = &thread::halt;
-		ops[0x38] = &thread::halt;
-		ops[0x39] = &thread::halt;
-		ops[0x3a] = &thread::halt;
-		ops[0x3b] = &thread::halt;
-		ops[0x3c] = &thread::halt;
-		ops[0x3d] = &thread::halt;
-		ops[0x3e] = &thread::halt;
-		ops[0x3f] = &thread::halt;
-		ops[0x40] = &thread::halt;
-		ops[0x41] = &thread::halt;
-		ops[0x42] = &thread::halt;
-		ops[0x43] = &thread::halt;
-		ops[0x44] = &thread::halt;
-		ops[0x45] = &thread::halt;
-		ops[0x46] = &thread::halt;
-		ops[0x47] = &thread::halt;
-		ops[0x48] = &thread::halt;
-		ops[0x49] = &thread::halt;
-		ops[0x4a] = &thread::halt;
-		ops[0x4b] = &thread::halt;
-		ops[0x4c] = &thread::halt;
-		ops[0x4d] = &thread::halt;
-		ops[0x4e] = &thread::halt;
-		ops[0x4f] = &thread::halt;
-		ops[0x50] = &thread::halt;
-		ops[0x51] = &thread::halt;
-		ops[0x52] = &thread::halt;
-		ops[0x53] = &thread::halt;
-		ops[0x54] = &thread::halt;
-		ops[0x55] = &thread::halt;
-		ops[0x56] = &thread::halt;
-		ops[0x57] = &thread::halt;
-		ops[0x58] = &thread::halt;
-		ops[0x59] = &thread::halt;
-		ops[0x5a] = &thread::halt;
-		ops[0x5b] = &thread::halt;
-		ops[0x5c] = &thread::halt;
-		ops[0x5d] = &thread::halt;
-		ops[0x5e] = &thread::halt;
-		ops[0x5f] = &thread::halt;
-		ops[0x60] = &thread::halt;
-		ops[0x61] = &thread::halt;
-		ops[0x62] = &thread::halt;
-		ops[0x63] = &thread::halt;
-		ops[0x64] = &thread::halt;
-		ops[0x65] = &thread::halt;
-		ops[0x66] = &thread::halt;
-		ops[0x67] = &thread::halt;
-		ops[0x68] = &thread::halt;
-		ops[0x69] = &thread::halt;
-		ops[0x6a] = &thread::halt;
-		ops[0x6b] = &thread::halt;
-		ops[0x6c] = &thread::halt;
-		ops[0x6d] = &thread::halt;
-		ops[0x6e] = &thread::halt;
-		ops[0x6f] = &thread::halt;
-		ops[0x70] = &thread::halt;
-		ops[0x71] = &thread::halt;
-		ops[0x72] = &thread::halt;
-		ops[0x73] = &thread::halt;
-		ops[0x74] = &thread::halt;
-		ops[0x75] = &thread::halt;
-		ops[0x76] = &thread::halt;
-		ops[0x77] = &thread::halt;
-		ops[0x78] = &thread::halt;
-		ops[0x79] = &thread::halt;
-		ops[0x7a] = &thread::halt;
-		ops[0x7b] = &thread::halt;
-		ops[0x7c] = &thread::halt;
-		ops[0x7d] = &thread::halt;
-		ops[0x7e] = &thread::halt;
-		ops[0x7f] = &thread::halt;
-		ops[0x80] = &thread::halt;
-		ops[0x81] = &thread::halt;
-		ops[0x82] = &thread::halt;
-		ops[0x83] = &thread::halt;
-		ops[0x84] = &thread::halt;
-		ops[0x85] = &thread::halt;
-		ops[0x86] = &thread::halt;
-		ops[0x87] = &thread::halt;
-		ops[0x88] = &thread::halt;
-		ops[0x89] = &thread::halt;
-		ops[0x8a] = &thread::halt;
-		ops[0x8b] = &thread::halt;
-		ops[0x8c] = &thread::halt;
-		ops[0x8d] = &thread::halt;
-		ops[0x8e] = &thread::halt;
-		ops[0x8f] = &thread::halt;
-		ops[0x90] = &thread::halt;
-		ops[0x91] = &thread::halt;
-		ops[0x92] = &thread::halt;
-		ops[0x93] = &thread::halt;
-		ops[0x94] = &thread::halt;
-		ops[0x95] = &thread::halt;
-		ops[0x96] = &thread::halt;
-		ops[0x97] = &thread::halt;
-		ops[0x98] = &thread::halt;
-		ops[0x99] = &thread::halt;
-		ops[0x9a] = &thread::halt;
-		ops[0x9b] = &thread::halt;
-		ops[0x9c] = &thread::halt;
-		ops[0x9d] = &thread::halt;
-		ops[0x9e] = &thread::halt;
-		ops[0x9f] = &thread::halt;
-		ops[0xa0] = &thread::halt;
-		ops[0xa1] = &thread::halt;
-		ops[0xa2] = &thread::halt;
-		ops[0xa3] = &thread::halt;
-		ops[0xa4] = &thread::halt;
-		ops[0xa5] = &thread::halt;
-		ops[0xa6] = &thread::halt;
-		ops[0xa7] = &thread::halt;
-		ops[0xa8] = &thread::halt;
-		ops[0xa9] = &thread::halt;
-		ops[0xaa] = &thread::halt;
-		ops[0xab] = &thread::halt;
-		ops[0xac] = &thread::halt;
-		ops[0xad] = &thread::halt;
-		ops[0xae] = &thread::halt;
-		ops[0xaf] = &thread::halt;
-		ops[0xb0] = &thread::halt;
-		ops[0xb1] = &thread::halt;
-		ops[0xb2] = &thread::halt;
-		ops[0xb3] = &thread::halt;
-		ops[0xb4] = &thread::halt;
-		ops[0xb5] = &thread::halt;
-		ops[0xb6] = &thread::halt;
-		ops[0xb7] = &thread::halt;
-		ops[0xb8] = &thread::halt;
-		ops[0xb9] = &thread::halt;
-		ops[0xba] = &thread::halt;
-		ops[0xbb] = &thread::halt;
-		ops[0xbc] = &thread::halt;
-		ops[0xbd] = &thread::halt;
-		ops[0xbe] = &thread::halt;
-		ops[0xbf] = &thread::halt;
-		ops[0xc0] = &thread::halt;
-		ops[0xc1] = &thread::halt;
-		ops[0xc2] = &thread::halt;
-		ops[0xc3] = &thread::halt;
-		ops[0xc4] = &thread::halt;
-		ops[0xc5] = &thread::halt;
-		ops[0xc6] = &thread::halt;
-		ops[0xc7] = &thread::halt;
-		ops[0xc8] = &thread::halt;
-		ops[0xc9] = &thread::halt;
-		ops[0xca] = &thread::halt;
-		ops[0xcb] = &thread::halt;
-		ops[0xcc] = &thread::halt;
-		ops[0xcd] = &thread::halt;
-		ops[0xce] = &thread::halt;
-		ops[0xcf] = &thread::halt;
-		ops[0xd0] = &thread::halt;
-		ops[0xd1] = &thread::halt;
-		ops[0xd2] = &thread::halt;
-		ops[0xd3] = &thread::halt;
-		ops[0xd4] = &thread::halt;
-		ops[0xd5] = &thread::halt;
-		ops[0xd6] = &thread::halt;
-		ops[0xd7] = &thread::halt;
-		ops[0xd8] = &thread::halt;
-		ops[0xd9] = &thread::halt;
-		ops[0xda] = &thread::halt;
-		ops[0xdb] = &thread::halt;
-		ops[0xdc] = &thread::halt;
-		ops[0xdd] = &thread::halt;
-		ops[0xde] = &thread::halt;
-		ops[0xdf] = &thread::halt;
-		ops[0xe0] = &thread::halt;
-		ops[0xe1] = &thread::halt;
-		ops[0xe2] = &thread::halt;
-		ops[0xe3] = &thread::halt;
-		ops[0xe4] = &thread::halt;
-		ops[0xe5] = &thread::halt;
-		ops[0xe6] = &thread::halt;
-		ops[0xe7] = &thread::halt;
-		ops[0xe8] = &thread::halt;
-		ops[0xe9] = &thread::halt;
-		ops[0xea] = &thread::halt;
-		ops[0xeb] = &thread::halt;
-		ops[0xec] = &thread::halt;
-		ops[0xed] = &thread::halt;
-		ops[0xee] = &thread::halt;
-		ops[0xef] = &thread::halt;
-		ops[0xf0] = &thread::halt;
-		ops[0xf1] = &thread::halt;
-		ops[0xf2] = &thread::halt;
-		ops[0xf3] = &thread::halt;
-		ops[0xf4] = &thread::halt;
-		ops[0xf5] = &thread::halt;
-		ops[0xf6] = &thread::halt;
-		ops[0xf7] = &thread::halt;
-		ops[0xf8] = &thread::halt;
-		ops[0xf9] = &thread::halt;
-		ops[0xfa] = &thread::halt;
-		ops[0xfb] = &thread::halt;
-		ops[0xfc] = &thread::print_int;
-		ops[0xfd] = &thread::print_float;
-		ops[0xfe] = &thread::print_char;
-		ops[0xff] = &thread::halt;
+		// 0x1.. : LOAD STORE
+		ops[0x11] = &thread::load_code;
+		ops[0x12] = &thread::load_code_array;
+		ops[0x13] = &thread::load_data;
+		ops[0x14] = &thread::load_data_array;
+		ops[0x15] = &thread::load_stack;
+		ops[0x16] = &thread::load_stack_array;
+		ops[0x17] = &thread::store_data;
+		ops[0x18] = &thread::store_data_array;
+		ops[0x19] = &thread::store_stack;
+		ops[0x1a] = &thread::store_stack_array;
+		// 0x02.. : STACK MANIPULATION
+		ops[0x20] = &thread::push;
+		ops[0x21] = &thread::push_array;
+		ops[0x22] = &thread::pop;
+		ops[0x23] = &thread::pop_array;
+		ops[0x24] = &thread::dup;
+		ops[0x25] = &thread::dup_array;
+		ops[0x26] = &thread::swap;
+		ops[0x27] = &thread::swap_array;
+		// 0x3.. : ARITHMETIC
+		ops[0x30] = &thread::iadd;
+		ops[0x31] = &thread::fadd;
+		ops[0x32] = &thread::isub;
+		ops[0x33] = &thread::fsub;
+		ops[0x34] = &thread::imul;
+		ops[0x35] = &thread::fmul;
+		ops[0x36] = &thread::idiv;
+		ops[0x37] = &thread::fdiv;
+		ops[0x38] = &thread::irem;
+		ops[0x39] = &thread::ineg;
+		ops[0x3a] = &thread::fneg;
+		// 0x4.. : TYPE CONVERSION
+		ops[0x40] = &thread::i2f;
+		ops[0x41] = &thread::f2i;
+		// 0x5.. : JUMP
+		ops[0x50] = &thread::jump;     
+		ops[0x51] = &thread::ifeq_jump;
+		ops[0x52] = &thread::iflt_jump;
+		ops[0x53] = &thread::ifgt_jump;
+		ops[0x54] = &thread::ifle_jump;
+		ops[0x55] = &thread::ifge_jump;
+		ops[0x56] = &thread::ifne_jump;
+		// 0x6.. : FUNCTION CALL
+		ops[0x60] = &thread::push_fp;
+		ops[0x61] = &thread::call_fp;
+		ops[0x62] = &thread::return0;
+		ops[0x63] = &thread::return1;
+		ops[0x64] = &thread::return1_array;
+		// 0xf.. : PRINT
+		ops[0xf0] = &thread::print_int;
+		ops[0xf1] = &thread::print_float;
+		ops[0xf2] = &thread::print_char;
 	}
 	~thread() {
 		std::free(stack);
@@ -456,15 +260,38 @@ private:
 		return 1 + 4 + 4;
 	}
 	// STACK MANIPULATION
+	// PUSH
+	stype push() {
+		stack_advance(+1);
+		return 1;
+	}
+	// PUSHARRAY LEN
+	stype push_array() {
+		utype len = c.fetch_param(ip + 1);
+		stack_advance(+ (stype)len);
+		return 1 + 4;
+	}
 	// POP
 	stype pop() {
-		stack_pop();
+		stack_advance(-1);
 		return 1;
+	}
+	// POPARRAY LEN
+	stype pop_array() {
+		utype len = c.fetch_param(ip + 1);
+		stack_advance(- (stype)len);
+		return 1 + 4;
 	}
 	// DUP
 	stype dup() {
 		stack_push(stack_peek());
 		return 1;
+	}
+	// DUPARRAY LEN
+	stype dup_array() {
+		utype len = c.fetch_param(ip + 1);
+		stack_memmove(sp, sp - len, len);
+		return 1 + 4;
 	}
 	// SWAP
 	stype swap() {
@@ -473,6 +300,16 @@ private:
 		stack_push(out1);
 		stack_push(out2);
 		return 1;
+	}
+	// SWAPARRAY LEN
+	stype swap_array() {
+		utype len = c.fetch_param(ip + 1);
+		utype *arr = (utype*)std::malloc(len * sizeof(utype));
+		std::memcpy(arr, stack_ptr() - len, len * sizeof(utype));
+		std::memcpy(stack_ptr() - len, stack_ptr() - 2*len, len * sizeof(utype));
+		std::memcpy(stack_ptr() - 2*len, arr, len * sizeof(utype));
+		std::free(arr);
+		return 1 + 4;
 	}
 	// ARITHMETIC
 	// helpers
@@ -671,6 +508,7 @@ private:
 		stack_advance(- (stype)len);
 		utype dst = stack_return();
 		std::memcpy(stack_ptr(), arr, len * sizeof(utype));
+		std::free(arr);
 		return ip_offset(dst) + 5;
 	}
 };
